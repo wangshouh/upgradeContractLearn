d4dbeface1f7a122edad8ee73d67d390b97b13f6
wangshouh <wongshouhao@outlook.com>
2022-07-18 21:52:40 +0800

    Change function name.

---
+++ src/EternalStorage/voteFirst.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract EternalStorage {
+    mapping(bytes32 => uint256) public UIntStorage;
+
+    function getUIntValue(bytes32 record) public view returns (uint256) {
+        return UIntStorage[record];
+    }
+
+    function setUIntValue(bytes32 record, uint256 value) public {
+        UIntStorage[record] = value;
+    }
+
+    mapping(bytes32 => bool) public BooleanStorage;
+
+    function getBooleanValue(bytes32 record) public view returns (bool) {
+        return BooleanStorage[record];
+    }
+
+    function setBooleanValue(bytes32 record, bool value) public {
+        BooleanStorage[record] = value;
+    }
+}
+
+contract VoteContract {
+    address eternalStorage;
+
+    constructor(address _eternalStorage) {
+        eternalStorage = _eternalStorage;
+    }
+
+    function getNumberOfVotes() public view returns(uint) {
+        return EternalStorage(eternalStorage).getUIntValue(keccak256('votes'));
+    }
+
+    function vote() public {
+        uint256 orgianVote = EternalStorage(eternalStorage).getUIntValue(keccak256('votes'));
+        EternalStorage(eternalStorage).setUIntValue(keccak256('votes'), orgianVote+1);
+    }
+}
