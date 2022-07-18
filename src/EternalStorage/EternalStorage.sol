868f12e73727decf289e1607204f085497d7b914
wangshouh <wongshouhao@outlook.com>
2022-07-18 19:34:21 +0800

    Add EternalStorage contract

--- src/EternalStorage/EternalStorage.sol
+++ src/EternalStorage/EternalStorage.sol
@@ -1,3 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+ contract EternalStorage {
+
+    mapping(bytes32 => uint) UIntStorage;
+
+    function getUIntValue(bytes32 record) public view returns (uint){
+        return UIntStorage[record];
+    }
+
+    function setUIntValue(bytes32 record, uint value) public
+    {
+        UIntStorage[record] = value;
+    }
+
+
+    mapping(bytes32 => bool) BooleanStorage;
+
+    function getBooleanValue(bytes32 record) public view returns (bool){
+        return BooleanStorage[record];
+    }
+
+    function setBooleanValue(bytes32 record, bool value) public
+    {
+        BooleanStorage[record] = value;
+    }
+
+
+}
