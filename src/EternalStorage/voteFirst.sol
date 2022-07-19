f8bf3b2479bc301e6f3dcbf4739f572a0c22d594
wangshouh <wongshouhao@outlook.com>
2022-07-19 11:43:50 +0800

    Change storage.

--- src/EternalStorage/voteFirst.sol
+++ src/EternalStorage/voteFirst.sol
@@ -11,16 +11,6 @@ contract EternalStorage {
     function setUIntValue(bytes32 record, uint256 value) public {
         UIntStorage[record] = value;
     }
-
-    mapping(bytes32 => bool) public BooleanStorage;
-
-    function getBooleanValue(bytes32 record) public view returns (bool) {
-        return BooleanStorage[record];
-    }
-
-    function setBooleanValue(bytes32 record, bool value) public {
-        BooleanStorage[record] = value;
-    }
 }
 
 contract VoteContract {
