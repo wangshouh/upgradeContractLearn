437be53d0a57eb045ae22d7980f5ca98333815c6
wangshouh <wongshouhao@outlook.com>
2022-07-20 11:02:07 +0800

    Add EternalStorage Test setUp

---
+++ test/EternalStorage/StoragteUpdate.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+// import "../../src/EternalStorage/EternalStorage.sol";
+import "../../src/EternalStorage/voteFirst.sol";
+import "../../src/EternalStorage/voteSecond.sol";
+
+contract ContractTest is Test {
+    EternalStorage private storageEth;
+    VoteFirst private voteFirst;
+    VoteSecond private voteSecond;
+
+    function setUp() public {
+        storageEth = new EternalStorage();
+        address storageAddress = address(storageEth);
+        emit log_address(storageAddress);
+        voteFirst = new VoteFirst(storageAddress);
+        voteSecond = new VoteSecond(storageAddress);
+    }
+
+    function testGetValueFromFirst() public {
+        voteFirst.vote();
+        uint256 voteNum = voteFirst.getNumberOfVotes();
+        assertEq(voteNum, 1);
+    }
+}
