de0192a30a78b0c82b570e86336f80fdbd2089d2
wangshouh <wongshouhao@outlook.com>
2022-07-20 20:21:27 +0800

    Add test.

--- test/EasyProxy/ProxyEasy.t.sol
+++ test/EasyProxy/ProxyEasy.t.sol
@@ -11,7 +11,56 @@ contract ContractTest is Test {
 
     function setUp() public {
         numberStorage = new NumberStorage();
+        numberStorageUp = new NumberStorageUp();
         address numberAddress = address(numberStorage);
         proxy = new ProxyEasy(numberAddress);
+    }
+
+    function testGetNumber() public {
+        (bool ok,) = address(proxy).call(
+            abi.encodeWithSignature("setNumber(uint256)", 100)
+        );
+        if (!ok) {
+            revert("DelegateCall Error");
+        }
+
+        (bool success, bytes memory data) = address(proxy).call(
+            abi.encodeWithSignature("getNumber()")
+        );
+
+        if (!success) {
+            revert("DelegateCall Error");
+        }
+        uint256 returnNumber = abi.decode(data, (uint256));
+        assertEq(returnNumber, 100);
+    }
+
+    function testNumberUpgrade() public {
+        (bool ok,) = address(proxy).call(
+            abi.encodeWithSignature("setNumber(uint256)", 100)
+        );
+        if (!ok) {
+            revert("DelegateCall Error");
+        }
+
+        proxy.setOtherAddress(address(numberStorageUp));
+
+        (bool addSuccess, ) = address(proxy).call(
+            abi.encodeWithSignature("addNumber()")
+        );
+
+        if (!addSuccess) {
+            revert("Add Fail");
+        }
+
+        (bool success, bytes memory data) = address(proxy).call(
+            abi.encodeWithSignature("getNumber()")
+        );
+
+        if (!success) {
+            revert("DelegateCall Error");
+        }
+        uint256 returnNumber = abi.decode(data, (uint256));
+        assertEq(returnNumber, 101);
     }
 }
