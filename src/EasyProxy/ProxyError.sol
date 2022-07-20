7993f7d13d9fd8f30a733cffa05d023ef2318df6
wangshouh <wongshouhao@outlook.com>
2022-07-20 18:00:35 +0800

    Add Error example.

---
+++ src/EasyProxy/ProxyError.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract NumberStorage {
+    uint256 public number;
+
+    function setNumber(uint256 _uint) public {
+        number = _uint;
+    }
+
+    function getNumber() public view returns (uint256) {
+        return number;
+    }
+}
+
+contract ProxyEasy {
+    address public otherContractAddress;
+
+    constructor(address _otherContract) {
+        otherContractAddress = _otherContract;
+    }
+
+    function setOtherAddress(address _otherContract) public {
+        otherContractAddress = _otherContract;
+    }
+
+    fallback() external {
+        address _impl = otherContractAddress;
+
+        assembly {
+            let ptr := mload(0x40)
+            calldatacopy(ptr, 0, calldatasize())
+            let result := delegatecall(gas(), _impl, ptr, calldatasize(), 0, 0)
+            let size := returndatasize()
+            returndatacopy(ptr, 0, size)
+
+            switch result
+            case 0 {
+                revert(ptr, size)
+            }
+            default {
+                return(ptr, size)
+            }
+        }
+    }
+}
