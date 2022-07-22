4a1d2496a534f27e8b3b107e6a0ecdd1a3ee88bc
wangshouh <wongshouhao@outlook.com>
2022-07-22 10:30:57 +0800

    Change fallbach implemention.

--- src/EIP-1822/EIP1822.sol
+++ src/EIP-1822/EIP1822.sol
@@ -20,16 +20,18 @@ contract Proxy {
     fallback() external payable {
         assembly { 
             let contractLogic := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
-            calldatacopy(0, 0, calldatasize())
-            let success := delegatecall(gas(), contractLogic, 0, calldatasize(), 0, 0)
-            let retSz := returndatasize()
-            returndatacopy(0, 0, retSz)
-            switch success
+            let ptr := mload(0x40)
+            calldatacopy(ptr, 0, calldatasize())
+            let result := delegatecall(gas(), contractLogic, ptr, calldatasize(), 0, 0)
+            let size := returndatasize()
+            returndatacopy(ptr, 0, size)
+
+            switch result
             case 0 {
-                revert(0, retSz)
+                revert(ptr, size)
             }
             default {
-                return(0, retSz)
+                return(ptr, size)
             }
         }
     }
