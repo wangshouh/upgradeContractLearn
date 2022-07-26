82c64a2d170b5fd90c8091834628e782433eaa88
wangshouh <wongshouhao@outlook.com>
2022-07-26 12:49:15 +0800

    Add changeAdim

--- src/EIP-1967/proxy.sol
+++ src/EIP-1967/proxy.sol
@@ -4,11 +4,17 @@ pragma solidity ^0.8.13;
 import "openzeppelin-contracts/contracts/proxy/beacon/BeaconProxy.sol";
 
 contract NFTProxy is BeaconProxy {
-    constructor(address beacon, bytes memory data) BeaconProxy(beacon, data) {}
+    constructor(address beacon, bytes memory data) BeaconProxy(beacon, data) {
+        _changeAdmin(msg.sender);
+    }
 
     modifier OnlyContractOwner() {
         require(msg.sender == _getAdmin(), "Not Contract Owner");
         _;
+    }
+
+    function getAdmin() public view {
+        _getAdmin();
     }
 
     function changeAdmin(address _newAdmin) public OnlyContractOwner {
