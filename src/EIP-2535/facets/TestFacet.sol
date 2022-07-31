f6ac5e81d337ec798a5f3cc78333cc3f9028fb4e
wangshouh <wongshouhao@outlook.com>
2022-07-31 11:54:18 +0800

    Add testfacet.

---
+++ src/EIP-2535/facets/TestFacet.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+pragma experimental ABIEncoderV2;
+
+import "../libraries/LibAppStorage.sol";
+
+contract TestFacet {
+
+    AppStorage internal s;
+    
+    function name() external view returns (string memory) {
+        return s.name;
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return s.totalSupply;
+    }
+
+    function maxSupply() external view returns (uint256) {
+        return s.maxSupply;
+    }
+}
