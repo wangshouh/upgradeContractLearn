f3e8d317d4f667d2fff98903c88f2718e9698f11
wangshouh <wongshouhao@outlook.com>
2022-08-07 18:13:29 +0800

    Add testSign.

--- test/EIP712/simple.t.sol
+++ test/EIP712/simple.t.sol
@@ -10,5 +10,23 @@ contract ContractTest is Test {
     function setUp() public {
         signContract = new Example();
     }
-    
+    function testSign() public {
+        // Example signed message
+        Example.Mail memory mail = Example.Mail({
+            from: Example.Person({
+               name: "Cow",
+               wallet: 0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826
+            }),
+            to: Example.Person({
+                name: "Bob",
+                wallet: 0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB
+            }),
+            contents: "Hello, Bob!"
+        });
+        uint8 v = 28;
+        bytes32 r = 0x4355c47d63924e8a72e509b65029052eb6c299d53a04e167c5775fd466751c9d;
+        bytes32 s = 0x07299936d304c153f6443dfa05f40ff007d72911b6f72307f996231605b91562;
+        
+        assertTrue(signContract.verify(mail, v, r, s));
+    }
 }
