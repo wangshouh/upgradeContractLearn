d05578a8a8caaba29597e6d75a3c2a26c211aa12
wangshouh <wongshouhao@outlook.com>
2022-08-12 16:39:11 +0800

    Add test nonce.

--- test/metatx/metatx.t.sol
+++ test/metatx/metatx.t.sol
@@ -54,4 +54,21 @@ contract MetaTxTest is Test {
         forwarder.execute(request, Signature);
         vm.stopPrank();
     }
+
+    function testNonce() public {
+        vm.chainId(4);
+        forwarder.execute(request, Signature);
+        bytes
+            memory NonceSign = hex"1fc1c1f112d94ea9a8a63ebe20aa97d9d5a5698c816c1588e6b0779cf297a2687f9149b7c67dd6e0c3455a040bca4151634382a114d6afdf1320b1c8c890ecd81b";
+        Forwarder.ForwardRequest memory requestNonce = Forwarder.ForwardRequest({
+            from: address(0x11475691C2CAA465E19F99c445abB31A4a64955C),
+            to: address(0x185a4dc360CE69bDCceE33b3784B0282f7961aea),
+            value: 0,
+            gas: 500000000000,
+            nonce: 1,
+            data: hex"6057361d0000000000000000000000000000000000000000000000000000000000000020"
+        });
+        forwarder.execute(requestNonce, NonceSign);
+        assertEq(recipient.retrieve(), 32);
+    }
 }
