794d50d65bd58619400c22e19827df151591e85f
wangshouh <wongshouhao@outlook.com>
2022-08-12 16:44:56 +0800

    Format forwarder

--- src/metatx/Forwarder.sol
+++ src/metatx/Forwarder.sol
@@ -15,17 +15,25 @@ contract Forwarder is Ownable, EIP712 {
         uint256 nonce;
         bytes data;
     }
-    bytes32 private constant _TYPEHASH = keccak256("ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data)");
-    
+    bytes32 private constant _TYPEHASH =
+        keccak256(
+            "ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data)"
+        );
+
     mapping(address => uint256) private _nonces;
     mapping(address => bool) private _senderWhitelist;
 
-    event MetaTransactionExecuted(address indexed from, address indexed to, bytes indexed data);
+    event MetaTransactionExecuted(
+        address indexed from,
+        address indexed to,
+        bytes indexed data
+    );
     event AddressWhitelisted(address indexed sender);
     event AddressRemovedFromWhitelist(address indexed sender);
 
-
-    constructor(string memory name, string memory version) EIP712(name, version) {
+    constructor(string memory name, string memory version)
+        EIP712(name, version)
+    {
         address msgSender = msg.sender;
         addSenderToWhitelist(msgSender);
     }
@@ -34,25 +42,47 @@ contract Forwarder is Ownable, EIP712 {
         return _nonces[from];
     }
 
-    function verify(ForwardRequest calldata req, bytes calldata signature) public view returns (bool) {
-        address signer = _hashTypedDataV4(keccak256(abi.encode(
-            _TYPEHASH,
-            req.from,
-            req.to,
-            req.value,
-            req.gas,
-            req.nonce,
-            keccak256(req.data)
-        ))).recover(signature);
+    function verify(ForwardRequest calldata req, bytes calldata signature)
+        public
+        view
+        returns (bool)
+    {
+        address signer = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    _TYPEHASH,
+                    req.from,
+                    req.to,
+                    req.value,
+                    req.gas,
+                    req.nonce,
+                    keccak256(req.data)
+                )
+            )
+        ).recover(signature);
         return _nonces[req.from] == req.nonce && signer == req.from;
     }
-    function execute(ForwardRequest calldata req, bytes calldata signature) public payable returns (bool, bytes memory) {
-        require(_senderWhitelist[msg.sender], "AwlForwarder: sender of meta-transaction is not whitelisted");
-        require(verify(req, signature), "AwlForwarder: signature does not match request");
+
+    function execute(ForwardRequest calldata req, bytes calldata signature)
+        public
+        payable
+        returns (bool, bytes memory)
+    {
+        require(
+            _senderWhitelist[msg.sender],
+            "AwlForwarder: sender of meta-transaction is not whitelisted"
+        );
+        require(
+            verify(req, signature),
+            "AwlForwarder: signature does not match request"
+        );
         _nonces[req.from] = req.nonce + 1;
 
-        (bool success, bytes memory returndata) = req.to.call{gas: req.gas, value: req.value}(abi.encodePacked(req.data, req.from));
-        
+        (bool success, bytes memory returndata) = req.to.call{
+            gas: req.gas,
+            value: req.value
+        }(abi.encodePacked(req.data, req.from));
+
         if (!success) {
             assembly {
                 let p := mload(0x40)
@@ -67,13 +97,17 @@ contract Forwarder is Ownable, EIP712 {
 
         return (success, returndata);
     }
-    function addSenderToWhitelist(address sender) public onlyOwner() {
-        require(!isWhitelisted(sender), "AwlForwarder: sender address is already whitelisted"); 
+
+    function addSenderToWhitelist(address sender) public onlyOwner {
+        require(
+            !isWhitelisted(sender),
+            "AwlForwarder: sender address is already whitelisted"
+        );
         _senderWhitelist[sender] = true;
         emit AddressWhitelisted(sender);
     }
 
-    function removeSenderFromWhitelist(address sender) public onlyOwner() {
+    function removeSenderFromWhitelist(address sender) public onlyOwner {
         _senderWhitelist[sender] = false;
         emit AddressRemovedFromWhitelist(sender);
     }
